f1507ee39ab9b8fe1af26485739cd07e731df610
ignisko <ignacyjanuszek@gmail.com>
2026-06-16 11:12:55 +0200

    Remove dynamic redirects breaking static build

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   redirects: {
     '/road2wyd': 'https://www.road2wyd.com/',
-    '/road2wyd/[...slug]': 'https://www.road2wyd.com/[...slug]',
     '/papaya': 'https://papaya-coral.vercel.app/',
-    '/papaya/[...slug]': 'https://papaya-coral.vercel.app/[...slug]',
   }
 });
