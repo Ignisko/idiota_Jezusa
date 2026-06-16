82603985c94ca51c56ece2212d792212e1981bce
ignisko <ignacyjanuszek@gmail.com>
2026-06-16 11:06:52 +0200

    Update Papaya redirect to the correct Vercel URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ export default defineConfig({
   redirects: {
     '/road2wyd': 'https://www.road2wyd.com/',
     '/road2wyd/[...slug]': 'https://www.road2wyd.com/[...slug]',
+    '/papaya': 'https://papaya-coral.vercel.app/',
+    '/papaya/[...slug]': 'https://papaya-coral.vercel.app/[...slug]',
   }
 });
